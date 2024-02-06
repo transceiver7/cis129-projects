07a0fc38b2c397879662983995c4b9098cbbcd5b
Jerry <158982064+transceiver7@users.noreply.github.com>
2024-02-06 16:03:02 -0600

    Update cis129_lab02_shortStory.py
    I have updated the code in the original cis129_lab02_shortStory.py file to include a mini bio.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+#Author:  Jerry Dartez
+#My first Python lab in the CIS 129 course
+#Prints a short story
+print('Hello, my name is Jerry Dartez')
+print('This is my first semester in the A.A.S. Programmer/Analyst program')
+print('I have an idea for a patent and hope to utilize programming to bring it to fruition')
+print('And now for something completely different:  my favorite band is Rush!')
