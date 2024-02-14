f9a889b1eebb494c46f4b33fe69787b0627f075b
Jerry <158982064+transceiver7@users.noreply.github.com>
2024-02-14 01:26:17 -0600

    Add files via upload
    This file is for Module 3: Lab.  It is a Coffee and Muffin Shop Simulator

---
+++ CIS_129_lab03_coffeeShop.py
@@ -0,0 +1,20 @@
+"""Module 3: Lab"""
+"""Coffee Shop Simulator"""
+"""Author:  Jerry Dartez"""
+"""CIS_129_lab03_coffeeShop.py"""
+print('My Coffee and Muffin Shop)
+      coffee=input('Number of Coffees bought')
+      muffin=input('Number of Muffins bought')
+      coffees=5
+      muffins=4
+print('My Coffee and Muffin Shop Receipt')
+print(coffee 'Coffee or Coffees', 'at', '$5', 'each:' '$', coffee*coffees)
+print(muffin 'Muffin or Muffins', 'at', '$4', 'each:' '$', muffin*muffins)
+total=(coffee*coffees)+(muffin*muffins)
+tax=total*1.06
+print('------------')
+print('Total: $', tax)
+print('**********************')
+      
+      
+      
